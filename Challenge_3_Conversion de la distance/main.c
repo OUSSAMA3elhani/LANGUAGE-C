6d5eb9b16db271a4a16997526a28dbd858cdb435
oussama el hani <oussamaelhani3@gmail.com>
2025-09-02 12:02:17 +0100

    challenge_4_Conversion de la vitesse_good

---
+++ Challenge_3_Conversion de la distance/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float km;
+    printf("donner  la distance en kilomètres :");
+    scanf("%f",&km);
+    printf("donner  la distance en yards :%f" ,km*1093.61);
+    return 0;
+}
