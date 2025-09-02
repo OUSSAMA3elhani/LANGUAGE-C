e46a2a0c9daf87292e8dc3eaa683b1bba1b7c529
oussama el hani <oussamaelhani3@gmail.com>
2025-09-02 15:15:10 +0100

    day2_Challenge_1_Paire_ou_Impaire_par_utllisation_de_'%'

---
+++ Day2_Challenge_1_Paire_ou_Impaire/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    printf("donner le nembre :");
+    scanf("%d",&a);
+    if(a%2==0)
+        printf("le nembre est pair");
+    else
+        printf("le nembre est impair");
+
+    return 0;
+}
