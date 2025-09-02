6f6282ddd0ad4fc12074468f94b8054a671452fc
oussama el hani <oussamaelhani3@gmail.com>
2025-09-02 11:28:06 +0100

    Challenge_1_Affichage_Informations_good

---
+++ Challenge_1_Affichage Informations/Challenge_1.c
@@ -0,0 +1,32 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    char nom[20],prénom[20],sexe[20],email[20];
+    int age;
+    printf("Donner le nom :");
+    scanf("%s",&nom);
+    printf("Donner le prénom :");
+    scanf("%s",&prénom);
+    printf("Donner l'age :");
+    scanf("%d",&age);
+    printf("Donner le sexe :");
+    scanf("%s",&sexe);
+    printf("Donner email :");
+    scanf("%s",&email);
+    printf("le nom est :%s\n",nom);
+    printf("le prénomest :%s\n",prénom);
+    printf("l'age est :%d\n",age);
+    printf("le sexe est :%s\n",sexe);
+    printf("l'email est :%s\n",email);
+    
+
+    
+    
+    
+    
+    
+
+
+    return 0;
+}
