efd4ff41772961b5b0d6cae02d7ac4298b975504
oussama el hani <oussamaelhani3@gmail.com>
2025-09-02 15:44:27 +0100

    Day2_Challenge_1_est_ce_que_le_caractére_est_Paire_ou_Impaire

---
+++ Challenge_2_Voyelle_ou_Non/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char c;
+    printf("donner le caractére :");
+    scanf("%c",&c);
+    switch(c){
+    case 'a':
+        printf("le caractére est voyelle:");
+        break;
+    case 'e':
+        printf("le caractére est voyelle:");
+        break;
+    case 'y':
+        printf("le caractére est voyelle:");
+        break;
+    case 'u':
+        printf("le caractére est voyelle:");
+        break;
+    case 'i':
+        printf("le caractére est voyelle:");
+        break;
+    case 'o':
+        printf("le caractére est voyelle:");
+        break;
+    case 'A':
+        printf("le caractére est voyelle:");
+        break;
+    case 'E':
+        printf("le caractére est voyelle:");
+        break;
+    case 'Y':
+        printf("le caractére est voyelle:");
+        break;
+    case 'U':
+        printf("le caractére est voyelle:");
+        break;
+    case 'I':
+        printf("le caractére est voyelle:");
+        break;
+    case 'O':
+        printf("le caractére est voyelle:");
+        break;
+    default:
+        printf("le caractére est constatnt:");
+        break;
+
+    }
+    return 0;
+}
