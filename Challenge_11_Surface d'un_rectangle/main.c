95e01959bd708fe46fbca4fe498b9f611b53baa8
oussama el hani <oussamaelhani3@gmail.com>
2025-09-02 14:10:57 +0100

    Challenge_11_calcul_de_la_surface

---
+++ Challenge_11_Surface d'un_rectangle/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float la,lo;
+    printf("donner la largeur :");
+    scanf("%f",&la);
+    printf("donner la longueur :");
+    scanf("%f",&lo);
+    printf("Surface:%f",la*lo);
+    return 0;
+}
